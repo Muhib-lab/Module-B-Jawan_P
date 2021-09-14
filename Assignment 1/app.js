b9206fd092475c84702af019ea47f2e83df6b758
Muhib-lab <muhibahmed1997@gmail.com>
2021-09-14 14:51:43 +0500

    PBR and PBV

---
+++ Assignment 1/app.js
@@ -0,0 +1,62 @@
+//...............PASS BY VALUE..............
+
+// const number = 6
+
+// function passByValue (a){
+//  return a += 10
+
+// }
+
+// console.log(passByValue(number))     //16
+
+// console.log(number)                 //6
+
+
+
+//.............PASS BY REFERENCE ...........
+
+// let student = ["name", "faher_name", "class"]
+
+// let student1 = student
+
+// student1.push("contact_num")
+
+// console.log(student1)    // ["name", "father_name", "class", "contact_num"]
+
+// console.log(student)      // ["name", "father_name", "class", "contact_num"]
+
+
+
+//..............CONCLUSION.........
+
+/*Understanding PBV vs PBR is helpful because if you are writing code 
+and you want to set one variable equal to another variable, or in the 
+example above, pass a variable into a function as an argument, you need 
+to know whether the value will be pass-by-value or pass-by-reference. 
+This will help you avoid errors when working with your data.*/
+
+
+
+
+
+//.................Template Literals 
+
+// var name = 'Basit Ahmed'
+// var standard = 18
+
+// console.log('My name is ' + name + 'and i read in class ' + standard)
+// console.log(`My name is ${name} and i read in class ${standard}`)
+
+
+//.........Ternary Operator................
+
+
+// 16 > 15 ? console.log('Greater') : console.log('Smaller')
+
+
+// let a
+// let age = 18
+
+// let cnic = a > 14 ? console.log('You age is less than 14') : console.log('your age is greater than 14')
+
+
